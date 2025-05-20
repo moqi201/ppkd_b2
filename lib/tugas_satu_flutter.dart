23dea8edfbf94f8e34da1c16df01846c09575275
moqi201 <nikihdyt1@gmail.com>
2025-05-20 10:57:29 +0700

    TUGAS 1 : FLUTER

--- lib/tugas_satu_flutter.dart
+++ lib/tugas_satu_flutter.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profil Saya',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
+      ),
+      home: ProfilPage(),
+    );
+  }
+}
+
 class ProfilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,52 +27,52 @@ class ProfilPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
       ),
-
-
-      body: Column(
-         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "profil saya ",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-                 SizedBox(height: 20),
-         Row(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(Icons.location_on, color: Colors.red,),
+            // Baris 1: Nama lengkap
             Text(
-              "Jl, MH THamrin ",
+              'NIKITA',
               style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w300,
-                color:Colors.black,
-              ),
-            )
-          ],
-         ),
-
-
-         Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              "saya adalah seorang gembala yang selalu riamg serta gembira kerena bapak saya garena",
-              style: TextStyle(
-                fontSize: 10,
+                fontSize: 26,
                 fontWeight: FontWeight.bold,
-                color: Colors.red,
+                color: Colors.black87,
               ),
             ),
+            SizedBox(height: 10),
+
+            // Baris 2: Lokasi
+            Row(
+              children: [
+                Icon(
+                  Icons.location_on,
+                  color: Colors.redAccent,
+                ),
+                SizedBox(width: 4),
+                Text(
+                  'SUDIRMAN',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[800],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            // Baris 3: Deskripsi singkat
+            Text(
+              'SAYA ADALAH BETMEN.',
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black54,
+              ),
+              textAlign: TextAlign.justify,
+            ),
           ],
-         ),
-
-
-
-
-        ],
+        ),
       ),
     );
   }
